f162a515c2ede0f6034dc660d69ac78cbf897ba4
Jose Lumbantobing <jolbntobing2@gmail.com>
2023-02-27 00:36:59 +0700

    Build HTML structure for rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import { useEffect } from "react";
+import SearchIcon from './search.svg';
 
 const API_KEY = 'http://www.omdbapi.com/?apikey=f7b5edc8';
+
+const movie = {
+    "Title": "Puss in Boots: The Last Wish",
+    "Year": "2022",
+    "imdbID": "tt3915174",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNjMyMDBjMGUtNDUzZi00N2MwLTg1MjItZTk2MDE1OTZmNTYxXkEyXkFqcGdeQXVyMTQ5NjA0NDM0._V1_SX300.jpg"
+}
 
 const App = () => {
     const searchMovies = async (title) => {
@@ -15,7 +24,27 @@ const App = () => {
     }, []);
 
     return (
-        <h1>App</h1>
+        <div className="movie-app">
+            <h1>MondoMovies</h1>
+
+            <div className="search-bar">
+                <input placeholder="Search movies..." />
+                <img src={SearchIcon} alt="search icon" />
+            </div>
+
+            <div className="movie-container">
+                <div className="movie-bar">
+                    <p>{movie.Year}</p>
+                </div>
+                <div>
+                    <img src={movie.Poster} alt={movie.Title} />
+                </div>
+                <div>
+                    <span>{movie.Type}</span>
+                    <h3>{movie.Title}</h3>
+                </div>
+            </div>
+        </div>
     );
 }
 
